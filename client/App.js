6194b1e00809c26e258048e09e939ed9582d204c
neubri <neubriasfruesty@gmail.com>
2025-08-23 13:21:39 +0700

    feat: implement authentication flow with login and registration screens

--- client/App.js
+++ client/App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { AuthProvider } from "./src/contexts/AuthContext";
+import AuthNavigator from "./src/navigators/AuthNavigator";
+import { RegisterScreen } from "./src/screens/RegisterScreen";
+import { LoginScreen } from "./src/screens/LoginScreen";
+import { useContext } from "react";
+import AuthContext from "./src/contexts/AuthContext";
+import { ActivityIndicator, View, StyleSheet } from "react-native";
+
+const Stack = createNativeStackNavigator();
+
+function AppNavigator() {
+  const { isSignedIn, isLoading } = useContext(AuthContext);
+
+  if (isLoading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#fff" />
+      </View>
+    );
+  }
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        {isSignedIn ? (
+          <Stack.Screen name="AuthNavigator" component={AuthNavigator} />
+        ) : (
+          <>
+            <Stack.Screen name="LoginScreen" component={LoginScreen} />
+            <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello word!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthProvider>
+      <AppNavigator />
+    </AuthProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  loadingContainer: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#000",
   },
 });
